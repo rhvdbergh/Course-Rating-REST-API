58a5725b9d2010296c2a76d75140d037db91a9bb
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-05-10 22:18:56 -0400

    add email validation

--- src/models.js
+++ src/models.js
@@ -4,10 +4,20 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
+function validateEmail(email) {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return regEx.test(email);
+
+};
+
 // Schema: User
 var UserSchema = new Schema({
     fullName: { type: String, required: true },
-    emailAddress: { type: String, required: true, unique: true }, // TODO: needs email validation
+    emailAddress: { 
+      type: String, 
+      required: true, 
+      unique: true,
+       }, // TODO: needs email validation
     password: {type: String, required: true }
   });
 
